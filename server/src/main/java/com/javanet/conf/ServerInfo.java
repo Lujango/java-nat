7cb99109b5d08339876e2bb77dfdcd6a96cb395a
Lujango <lujango@yahoo.com>
2023-02-24 17:09:21 +0800

    ServerInfo.java

--- server/src/main/java/com/javanet/conf/ServerInfo.java
+++ server/src/main/java/com/javanet/conf/ServerInfo.java
@@ -1,5 +1,27 @@
 package com.javanet.conf;
 
+import java.util.List;
+
+/**
+ * serverinfo
+ */
 public class ServerInfo {
     private String clientName;
+    private List<ServerWorker> serverWorkers;
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public List<ServerWorker> getServerWorkers() {
+        return serverWorkers;
+    }
+
+    public void setServerWorkers(List<ServerWorker> serverWorkers) {
+        this.serverWorkers = serverWorkers;
+    }
 }
