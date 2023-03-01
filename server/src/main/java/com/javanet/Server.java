c2ffe224d78535931dc1516eb4dfd9b3d957f9bf
Lujango <lujango@yahoo.com>
2023-03-01 16:35:16 +0800

    Server

---
+++ server/src/main/java/com/javanet/Server.java
@@ -0,0 +1,12 @@
+package com.javanet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * listen port start server
+ * no autoRead
+ */
+public class Server {
+    private static Logger log = LoggerFactory.getLogger(Server.class);
+}
