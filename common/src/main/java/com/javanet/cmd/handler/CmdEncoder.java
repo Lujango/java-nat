b7717deb91046c1f30b4cc367dad26bd5b2a6d93
Lujango <lujango@yahoo.com>
2023-04-16 09:22:49 +0800

    CmdEncoder

--- common/src/main/java/com/javanet/cmd/handler/CmdEncoder.java
+++ common/src/main/java/com/javanet/cmd/handler/CmdEncoder.java
@@ -1,7 +1,20 @@
 package com.javanet.cmd.handler;
 
+import com.javanet.cmd.message.Cmd;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
 /**
  * cmd encoder
  */
-public class CmdEncoder {
+public class CmdEncoder extends MessageToByteEncoder<Cmd> {
+
+    //输出
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Cmd msg, ByteBuf out) {
+        msg.encoderTo(out);
+    }
+
+
 }
