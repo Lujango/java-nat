5700f6cc6d6954f998f80e2bd27e2e4edf26dc39
Lujango <lujango@yahoo.com>
2023-04-17 09:40:44 +0800

    Cmd

--- common/src/main/java/com/javanet/cmd/message/Cmd.java
+++ common/src/main/java/com/javanet/cmd/message/Cmd.java
@@ -1,4 +1,28 @@
 package com.javanet.cmd.message;
 
-public interface Cmd {
+import io.netty.buffer.ByteBuf;
+
+public  interface Cmd {
+
+    byte dataConnectionCmd = 0;
+    byte ping = 4;
+    byte pong = 6;
+
+
+    interface ServerToClient {
+        byte serverStartSuccessCmd = 2;
+        byte serverStartFailCmd = 3;
+        byte loginFail = 7;
+    }
+
+    interface ClientToServer {
+        byte login = 5;
+    }
+
+
+    /*
+     * 将当前对象序列化到Bytebuf中
+     * */
+    void encoderTo(ByteBuf buf);
+
 }
