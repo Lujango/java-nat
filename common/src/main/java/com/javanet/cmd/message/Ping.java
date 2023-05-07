ffa70ad85df905615910d29d4b812d40fd0e66bd
Lujango <lujango@yahoo.com>
2023-05-07 20:47:26 +0800

    Ping

--- common/src/main/java/com/javanet/cmd/message/Ping.java
+++ common/src/main/java/com/javanet/cmd/message/Ping.java
@@ -1,4 +1,18 @@
 package com.javanet.cmd.message;
 
-public class Ping {
+import io.netty.buffer.ByteBuf;
+
+public class Ping implements Cmd {
+
+    private static Ping instance = new Ping();
+
+    @Override
+    public void encoderTo(ByteBuf buf) {
+        buf.writeByte(Cmd.ping);
+    }
+
+    public static Ping decoderFrom(ByteBuf in) {
+        return instance;
+    }
+
 }
