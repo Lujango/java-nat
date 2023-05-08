5aeb2b227bd76414c9d63eedc00b072ee32e7c77
Lujango <lujango@yahoo.com>
2023-05-08 09:06:13 +0800

    Pong

--- common/src/main/java/com/javanet/cmd/message/Pong.java
+++ common/src/main/java/com/javanet/cmd/message/Pong.java
@@ -1,4 +1,18 @@
 package com.javanet.cmd.message;
 
-public class Pong {
+import io.netty.buffer.ByteBuf;
+
+public class Pong implements Cmd {
+
+    private static Pong instance = new Pong();
+
+    @Override
+    public void encoderTo(ByteBuf buf) {
+        buf.writeByte(Cmd.pong);
+    }
+
+    public static Pong decoderFrom(ByteBuf in) {
+        return instance;
+    }
+
 }
