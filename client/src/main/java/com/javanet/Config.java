72a1784ef8d1e7b4437c7ecf43f34568dd164ffa
Lujango <lujango@yahoo.com>
2023-05-13 11:47:09 +0800

    Config

--- client/src/main/java/com/javanet/Config.java
+++ client/src/main/java/com/javanet/Config.java
@@ -1,4 +1,54 @@
 package com.javanet;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class Config {
+    private String serverIp;
+    private int serverPort;
+    private String clientName;
+    private String token;
+
+    public static Config INSTANCE = new Config();
+
+    private Config() {
+        try {
+            InputStream in = ClassLoader.getSystemResourceAsStream("config.properties");
+            if (in == null) {
+                if (Files.notExists(Paths.get("config.properties"))) {
+                    throw new RuntimeException("Unable to find configuration file : config.properties");
+                }
+                in = new FileInputStream("config.properties");
+            }
+
+            Properties prop = new Properties();
+            prop.load(in);
+            serverIp = prop.getProperty("serverIp");
+            clientName = prop.getProperty("clientName");
+            token = prop.getProperty("token");
+            serverPort = Integer.parseInt(prop.getProperty("serverPort"));
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getServerIp() {
+        return serverIp;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
