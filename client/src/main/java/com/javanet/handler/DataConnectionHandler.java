14724b636cfab31885ab1ec32d38e0317a8109d3
Lujango <lujango@yahoo.com>
2023-05-16 08:53:06 +0800

    DataConnectionHandler

--- client/src/main/java/com/javanet/handler/DataConnectionHandler.java
+++ client/src/main/java/com/javanet/handler/DataConnectionHandler.java
@@ -1,4 +1,28 @@
 package com.javanet.handler;
 
-public class DataConnectionHandler {
+import com.javanet.AppClient;
+import com.javanet.Config;
+import com.javanet.DataConnectionClient;
+import com.javanet.cmd.message.DataConnectionCmd;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.Attribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataConnectionHandler extends SimpleChannelInboundHandler<DataConnectionCmd> {
+
+    private Logger log = LoggerFactory.getLogger(DataConnectionHandler.class);
+
+    /*
+     * It should be noted that this ctx belongs to the managerChannel and cannot be closed even if the activation fails
+     * */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, DataConnectionCmd msg) {
+        Attribute<Config> conf = ctx.channel().attr(AppClient.key);
+        Config config = conf.get();
+        log.debug("Received from the server DataConnection,{} -> {}:{}", msg.getServerPort(), msg.getLocalHost(), msg.getLocalPort());
+        new DataConnectionClient(config, msg).start();
+    }
+
 }
