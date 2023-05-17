4dc9c66a3882bdbe26ff36d6dc8c004760821517
Lujango <lujango@yahoo.com>
2023-05-17 09:38:18 +0800

    LoginFailHandler

--- client/src/main/java/com/javanet/handler/LoginFailHandler.java
+++ client/src/main/java/com/javanet/handler/LoginFailHandler.java
@@ -1,4 +1,17 @@
 package com.javanet.handler;
 
-public class LoginFailHandler {
+import com.javanet.cmd.message.serverToClient.LoginFailCmd;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginFailHandler  extends SimpleChannelInboundHandler<LoginFailCmd> {
+    private static Logger log = LoggerFactory.getLogger(LoginFailCmd.class);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, LoginFailCmd msg) throws Exception {
+        log.info(msg.toString());
+        ctx.close();
+    }
 }
