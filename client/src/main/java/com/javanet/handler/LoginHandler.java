06286b3861e98f110c44f761f51a82bef5c0f3ee
Lujango <lujango@yahoo.com>
2023-05-20 09:13:23 +0800

    LoginHandler

--- client/src/main/java/com/javanet/handler/LoginHandler.java
+++ client/src/main/java/com/javanet/handler/LoginHandler.java
@@ -1,4 +1,17 @@
 package com.javanet.handler;
 
-public class LoginHandler {
+import com.javanet.AppClient;
+import com.javanet.Config;
+import com.javanet.cmd.message.clientToServer.LoginCmd;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class LoginHandler  extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Config config = ctx.channel().attr(AppClient.key).get();
+        ctx.writeAndFlush(new LoginCmd(config.getClientName()));
+        super.channelActive(ctx);
+    }
 }
