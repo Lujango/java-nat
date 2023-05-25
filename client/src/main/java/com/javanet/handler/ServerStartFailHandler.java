394dfa33483ec2fda2c59b2d08a3262f878720d5
Lujango <lujango@yahoo.com>
2023-05-25 09:05:31 +0800

    ServerStartFailHandler

--- client/src/main/java/com/javanet/handler/ServerStartFailHandler.java
+++ client/src/main/java/com/javanet/handler/ServerStartFailHandler.java
@@ -1,4 +1,21 @@
 package com.javanet.handler;
 
-public class ServerStartFailHandler {
+import com.javanet.cmd.message.serverToClient.ServerStartFailCmd;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerStartFailHandler  extends SimpleChannelInboundHandler<ServerStartFailCmd> {
+
+    private Logger log = LoggerFactory.getLogger(ServerStartFailHandler.class);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ServerStartFailCmd msg) {
+        log.error(msg.toString());
+        if (ctx.channel().isActive()) {
+            ctx.close();
+        }
+    }
+
 }
