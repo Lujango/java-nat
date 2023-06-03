d19919252d2bed528d36147006de842ac58f30e4
Lujango <lujango@yahoo.com>
2023-06-03 21:37:00 +0800

    ServerStartSuccessHandler

--- client/src/main/java/com/javanet/handler/ServerStartSuccessHandler.java
+++ client/src/main/java/com/javanet/handler/ServerStartSuccessHandler.java
@@ -1,4 +1,18 @@
 package com.javanet.handler;
 
-public class ServerStartSuccessHandler {
+import com.javanet.cmd.message.serverToClient.ServerStartSuccessCmd;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerStartSuccessHandler extends SimpleChannelInboundHandler<ServerStartSuccessCmd> {
+
+    private Logger log = LoggerFactory.getLogger(ServerStartSuccessHandler.class);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ServerStartSuccessCmd msg) {
+        log.info(msg.toString());
+    }
+
 }
