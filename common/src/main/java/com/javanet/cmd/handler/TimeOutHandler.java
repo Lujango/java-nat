253529d1099bee22cc4b3b7ec53cdb4432fe3c9a
Lujango <lujango@yahoo.com>
2023-06-04 08:54:54 +0800

    TimeOutHandler

--- common/src/main/java/com/javanet/cmd/handler/TimeOutHandler.java
+++ common/src/main/java/com/javanet/cmd/handler/TimeOutHandler.java
@@ -1,4 +1,23 @@
 package com.javanet.cmd.handler;
 
-public class TimeOutHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimeOutHandler  extends IdleStateHandler {
+
+    private static Logger log = LoggerFactory.getLogger(TimeOutHandler.class);
+
+    public TimeOutHandler(int readerIdleTimeSeconds, int writerIdleTimeSeconds, int allIdleTimeSeconds) {
+        super(readerIdleTimeSeconds, writerIdleTimeSeconds, allIdleTimeSeconds);
+    }
+
+
+    @Override
+    public final void channelIdle(ChannelHandlerContext ctx, IdleStateEvent evt) throws Exception {
+        log.debug("timeout offline :{},Event:{}", ctx, evt);
+        ctx.flush().close();
+    }
 }
