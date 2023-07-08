2f6fc49c5fff56b66eb31f71596ee93611a4d369
Lujango <lujango@yahoo.com>
2023-07-08 08:24:57 +0800

    AppServer

--- server/src/main/java/com/javanet/AppServer.java
+++ server/src/main/java/com/javanet/AppServer.java
@@ -1,11 +1,65 @@
 package com.javanet;
 
 
+import com.javanet.cmd.handler.CmdEncoder;
+import com.javanet.cmd.handler.Rc4Md5Handler;
+import com.javanet.cmd.handler.TimeOutHandler;
+import com.javanet.handler.ServerCmdDecoder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 /**
  * server
  */
 public class AppServer {
-    public static void main(String[] args) {
 
+    private static Logger log = LoggerFactory.getLogger(AppServer.class);
+    public static EventLoopGroup boss = new NioEventLoopGroup(1);
+    public static EventLoopGroup work = new NioEventLoopGroup(1);
+
+    /*
+     * -c config.properties
+     * */
+    public static void main(String[] args) throws IOException {
+        Config.init("config.json");
+        ServerBootstrap sb = new ServerBootstrap();
+        sb.group(boss, work)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_RCVBUF, 32 * 1024)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 6000)
+                .childOption(ChannelOption.SO_RCVBUF, 128 * 1024)
+                .childOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, 6000)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline().addLast(new Rc4Md5Handler(Config.token));
+                        ch.pipeline().addLast(new CmdEncoder());
+                        //控制超时，防止链接上来但不发送消息任何的连接
+                        ch.pipeline().addLast(new TimeOutHandler(0, 0, 360));
+                        ch.pipeline().addLast(new ServerCmdDecoder());
+                    }
+                })
+                .bind(Config.bindHost, Config.bindPort)
+                .addListener((ChannelFutureListener) future -> {
+                    if (future.isSuccess()) {
+                        log.info("服务端在端口:{}启动成功", Config.bindPort);
+                    } else {
+                        log.error("服务端在端口:" + Config.bindPort + "启动失败", future.cause());
+                        stop();
+                    }
+                });
     }
+
+    private static void stop() {
+        boss.shutdownGracefully();
+        work.shutdownGracefully();
+    }
+
 }
